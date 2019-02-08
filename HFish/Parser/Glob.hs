3b8cb893a8d4408a131bfb105e0596f752af089c
Dominik Xaver Hörl <hoe.dom@gmx.de>
2019-02-08 19:39:37 +0100

    The ? wildcard has been removed

--- HFish/Parser/Glob.hs
+++ HFish/Parser/Glob.hs
@@ -10,10 +10,9 @@ import Control.Applicative
 import Control.Monad
 
 glob :: P m => m Glob
-glob = qmark <|> star <?> "glob-pattern"
+glob = stars <?> "glob-pattern"
   where
-    qmark = char '?' $> QMarkGl
-    star =
+    stars =
       char '*'
       *> option StarGl (char '*' $> DiStarGl)
 
